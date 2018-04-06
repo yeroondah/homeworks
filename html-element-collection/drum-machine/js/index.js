f2c03b04ed9b46e97b5508010d93dc17686311c3
Vladimir <vl.scherbitskiy@gmail.com>
2018-04-06 22:06:24 +0300

    Изменение:при повторном клике, звуки воспроизводятся заново

--- html-element-collection/drum-machine/js/index.js
+++ html-element-collection/drum-machine/js/index.js
@@ -6,7 +6,12 @@ for (let drum of drums) {
   drum.onclick = function() {
     let sound = drum.getElementsByTagName('audio');
     for (let play of sound) {
-      play.play();
+      if (play.paused) {
+        play.play();
+      }
+      else {
+        play.currentTime = 0;
+      } 
     }
   }
 }
