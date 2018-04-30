1a097086da866cde927ac663490b5dbd6f3d3997
Vladimir <vl.scherbitskiy@gmail.com>
2018-04-30 14:02:54 +0300

    добавлен код

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,21 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.tagName !== 'TH') {
+    return;
+  }
+
+  const currentTh = event.target;
+  const propName = currentTh.dataset.propName;
+
+  table.dataset.sortBy = propName;
+
+  if (currentTh.dataset.dir === undefined || currentTh.dataset.dir === '-1') {
+    currentTh.dataset.dir = '1';
+  }
+  else {
+    currentTh.dataset.dir = '-1';
+  }
+
+  sortTable(propName, currentTh.dataset.dir);
 }
