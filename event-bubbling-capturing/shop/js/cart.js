ccbc5bc93ba3e4cebb534585c1115203adad720e
Vladimir <vl.scherbitskiy@gmail.com>
2018-04-30 13:49:53 +0300

    добавлен код

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,18 @@
 'use strict';
+
+list.addEventListener('click', cartLoader);
+
+function cartLoader(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('add-to-cart')) {
+    return;
+  }
+
+  const itemInfo = {
+    title: event.target.dataset.title,
+    price: event.target.dataset.price
+  };
+
+  addToCart(itemInfo);
+}
