afe196677bb15c93182fb1efcfc658ceb7868b15
Vladimir <vl.scherbitskiy@gmail.com>
2018-05-07 15:16:27 +0300

    добавлен код в файл counter.js

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const webSocket = new WebSocket('wss://neto-api.herokuapp.com/counter');
+const counterOutput = document.querySelector('.counter');
+const errorsOutput = document.querySelector('.errors');
+
+window.addEventListener('beforeunload', () => {
+  webSocket.close(1000, 'Работа закончена.');
+});
+
+webSocket.addEventListener('message', (event) => {
+  const data = JSON.parse(event.data);
+  counterOutput.textContent = data.connections;
+  errorsOutput.textContent = data.errors;
+});
