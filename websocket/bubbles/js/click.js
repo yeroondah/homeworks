ac860af2ae2d0642f929695c9239830ce4030be1
Vladimir <vl.scherbitskiy@gmail.com>
2018-05-07 15:05:29 +0300

    добавлен код в файл click.js

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connection.addEventListener('open', (event) => {
+  showBubbles(event.target);
+});
+
+document.addEventListener('click', (event) => {
+  connection.send(JSON.stringify({
+    x: event.clientX,
+    y: event.clientY
+  }));
+});
