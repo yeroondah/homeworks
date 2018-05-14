59bc661739d6f12e66bdbbd1f87688c5dd857970
Vladimir <vl.scherbitskiy@gmail.com>
2018-05-14 12:06:53 +0300

    добавлен код в файл twitter.js

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const wallpaper = document.querySelector('[data-wallpaper]');
+const username = document.querySelector('[data-username]');
+const description = document.querySelector('[data-description]');
+const pic = document.querySelector('[data-pic]');
+const tweets = document.querySelector('[data-tweets]');
+const followers = document.querySelector('[data-followers]');
+const following = document.querySelector('[data-following]');
+
+let getData = document.createElement('script');
+getData.src = 'https://neto-api.herokuapp.com/twitter/jsonp?callback=handleData';
+document.head.appendChild(getData);
+
+function handleData(data) {
+  username.textContent = data.username;
+  description.textContent = data.description;
+  tweets.textContent = data.tweets;
+  followers.textContent = data.followers;
+  following.textContent = data.following;
+  wallpaper.src = data.wallpaper;
+  pic.src = data.pic;
+}
