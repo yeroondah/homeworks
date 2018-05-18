5e992aa0f5c1165ef32510bc5b63c7d3a8bdd617
Vladimir <vl.scherbitskiy@gmail.com>
2018-05-18 13:33:44 +0300

    решение домашнего задания

--- html-forms/todo-list/js/task.js
+++ html-forms/todo-list/js/task.js
@@ -5,25 +5,25 @@ const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 const taskList = document.querySelector('.list-block');
 let doneTask = 0;
 Array.from(checkboxes).forEach(checkbox => {
-	if(checkbox.checked === true) {
-		taskCounter.value = ++doneTask;
-	}
+  if(checkbox.checked === true) {
+    taskCounter.value = ++doneTask;
+  }
 
   checkbox.addEventListener('change', () => {
-		if(checkbox.checked === true) {
-			taskCounter.value = ++doneTask;
-		}
-		else {
-			taskCounter.value = --doneTask;
-		}
-	});
+    if(checkbox.checked === true) {
+      taskCounter.value = ++doneTask;
+    }
+    else {
+      taskCounter.value = --doneTask;
+    }
+  });
 
-	checkbox.addEventListener('change', () => {
-		if (Array.from(checkboxes).length === doneTask) {
-			taskList.classList.add('complete');
-		}
-		else {
-			taskList.classList.remove('complete');
-		}
-	});
+  checkbox.addEventListener('change', () => {
+    if (Array.from(checkboxes).length === doneTask) {
+      taskList.classList.add('complete');
+    }
+    else {
+      taskList.classList.remove('complete');
+    }
+  });
 });
