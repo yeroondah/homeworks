38f32482085de18dbd5add7dd7d04e7fd2de4040
Vladimir <vl.scherbitskiy@gmail.com>
2018-05-19 13:22:55 +0300

    homework

--- websocket/chat/js/chat.js
+++ websocket/chat/js/chat.js
@@ -1,1 +1,75 @@
 'use strict';
+
+const chat = document.querySelector('.chat');
+const chatElems = init(chat);
+const ws = new WebSocket('wss://neto-api.herokuapp.com/chat');
+
+chatElems.content.setAttribute('style', 'overflow-y: auto;');
+
+ws.addEventListener('open', () => {
+  triggerStatus('Пользователь появился в сети');
+  chatElems.status.textContent = chatElems.status.dataset.online;
+  chatElems.submit.disabled = false;
+});
+
+ws.addEventListener('close', () => {
+  triggerStatus('Пользователь не в сети');
+  chatElems.status.textContent = chatElems.status.dataset.offline;
+  chatElems.submit.disabled = true;
+});
+
+ws.addEventListener('message', (event) => {
+  const data = event.data;
+  const typingMessage = chatElems.templates.loading;
+  const messageUser = chatElems.templates.messageUser.cloneNode(true);
+
+  if (data === '...') {
+    chatElems.content.appendChild(typingMessage).scrollIntoView({block: "end", behavior: "smooth"});
+  } else {
+    addMessage(messageUser, data);
+    chatElems.content.removeChild(typingMessage);
+    chatElems.content.appendChild(messageUser).scrollIntoView({block: "end", behavior: "smooth"});
+  }
+});
+
+chatElems.form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const value = chatElems.input.value;
+  const messageMy = chatElems.templates.messageMy.cloneNode(true);
+
+  addMessage(messageMy,value);
+  chatElems.content.appendChild(messageMy).scrollIntoView({block: "end", behavior: "smooth"});
+  ws.send(value);
+  chatElems.form.reset();
+});
+
+function init(chatElem) {
+  return {
+    form: chatElem.querySelector('.message-box'),
+    input: chatElem.querySelector('.message-input'),
+    submit: chatElem.querySelector('.message-submit'),
+    content:chatElem.querySelector('.messages-content'),
+    status: chatElem.querySelector('.chat-status'),
+    templates: {
+      messageUser: chatElem.querySelector('[class="message"]'),
+      messageMy: chatElem.querySelector('.message.message-personal'),
+      loading: chatElem.querySelector('.message.loading').cloneNode(true),
+      status: chatElem.querySelector('.message.message-status')
+    }
+  };
+}
+
+function triggerStatus(messageStatus) {
+  const message = chatElems.templates.status.cloneNode(true);
+  message.querySelector('.message-text').textContent = messageStatus;
+  chatElems.content.appendChild(message);
+}
+
+function addMessage(who, data) {
+  const time = new Date();
+  const hour = time.getHours() < 10 ? `0${time.getHours()}` : time.getHours();
+  const minutes = time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes();
+  who.querySelector('.message-text').textContent = data;
+  who.querySelector('.timestamp').textContent = `${hour}:${minutes}`;
+}
--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,12 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
+request.addEventListener('load', loadData);
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', true);
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
+
+function loadData(event) {
+  console.log(event.target);
+  if (event.target.status === 200) {
+    const response = JSON.parse(event.target.responseText);
+    setData(response);
+  }
 }
