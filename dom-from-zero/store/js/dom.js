d63d8561086e403f55d0eac095a0393c97aaa239
Vladimir <vl.scherbitskiy@gmail.com>
2018-05-21 23:01:57 +0300

    решение домашнего задания

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+
+  const el = document.createElement(node.name);
+
+  if (typeof node.props === 'object' && node.props !== null) {
+    Object.keys(node.props).forEach(i => el.setAttribute(i, node.props[i]));
+  }
+
+  if (node.childs instanceof Array) {
+    node.childs.forEach(child => {
+      const childEl = createElement(child);
+      el.appendChild(childEl);
+    });
+  }
+
+  return el;
+}
