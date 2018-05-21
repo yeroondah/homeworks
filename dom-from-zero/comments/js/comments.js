721f0bcf73f4ffd6aaa43eff2f97bd0c09a93a8e
Vladimir <vl.scherbitskiy@gmail.com>
2018-05-21 22:44:22 +0300

    решение домашнего задания

--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -2,28 +2,51 @@
 
 function showComments(list) {
   const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+  const comments = list.map(createComment);
+
+  comments.forEach(item => commentsContainer.appendChild(item));
+}
+
+function el(tagName, attributes, children) {
+  const element = document.createElement(tagName);
+
+  if (typeof attributes === 'object') {
+    Object.keys(attributes).forEach(i => element.setAttribute(i, attributes[i]));
+  }
+
+  if (typeof children === 'string') {
+    element.textContent = children;
+  } else if (children instanceof Array) {
+    children.forEach(child => element.appendChild(child));
+  }
+
+  return element;
 }
 
 function createComment(comment) {
-  return `<div class="comment-wrap">
-    <div class="photo" title="${comment.author.name}">
-      <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
-    </div>
-    <div class="comment-block">
-      <p class="comment-text">
-        ${comment.text.split('\n').join('<br>')}
-      </p>
-      <div class="bottom-comment">
-        <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
-        <ul class="comment-actions">
-          <li class="complain">Пожаловаться</li>
-          <li class="reply">Ответить</li>
-        </ul>
-      </div>
-    </div>
-  </div>`
+  const textList = comment.text.split('\n');
+
+  return el('div', {class: 'comment-wrap'}, [
+    el('div', {class: 'photo', title: comment.author.name}, [
+      el('div', {class: 'avatar', style: `background-image: url('${comment.author.pic}')`})
+    ]),
+    el('div', {class: 'comment-block'}, [
+      el('p', {class: 'comment-text'}, textList.map(item => {
+        const br = el('br');
+        const span = el('span');
+        span.textContent = item;
+        span.appendChild(br);
+        return span;
+      })),
+      el('div', {class: 'bottom-comment'}, [
+        el('div', {class: 'comment-date'}, new Date(comment.date).toLocaleString('ru-Ru')),
+        el('ul', {class: 'comment-actions'}, [
+          el('li', {class: 'complain'}, 'Пожаловаться'),
+          el('li', {class: 'reply'}, 'Ответить')
+        ])
+      ])
+    ])
+  ]);
 }
 
 fetch('https://neto-api.herokuapp.com/comments')
