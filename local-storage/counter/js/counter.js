6585bb2d6f2c882b0daf1177c1e9e94b1e4c5cfc
Vladimir <vl.scherbitskiy@gmail.com>
2018-05-26 22:13:51 +0300

    решение домашнего задания

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const counter = document.getElementById('counter');
+const btnInc = document.getElementById('increment');
+const btnDec = document.getElementById('decrement');
+const btnReset = document.getElementById('reset');
+
+if (sessionStorage.countValue === undefined) {
+  sessionStorage.countValue = 0;
+}
+
+counter.textContent = sessionStorage.countValue;
+
+btnInc.addEventListener('click', () => {
+  counter.textContent = ++sessionStorage.countValue;
+});
+
+btnDec.addEventListener('click', () => {
+  counter.textContent = sessionStorage.countValue <= 0 ? 0 : --sessionStorage.countValue;
+});
+
+btnReset.addEventListener('click', () => {
+  sessionStorage.countValue = 0;
+  counter.textContent = 0;
+});
