889891614e359c9876fd56f1ab6ea6b296082c6d
Vladimir <vl.scherbitskiy@gmail.com>
2018-05-26 22:18:29 +0300

    решение домашнего задания

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const formSignIn = document.querySelector('.sign-in-htm');
+const formSignUp = document.querySelector('.sign-up-htm');
+const messageSignIn = formSignIn.querySelector('.error-message');
+const messageSignUp = formSignUp.querySelector('.error-message');
+
+initFormSubmit(formSignIn, 'https://neto-api.herokuapp.com/signin', messageSignIn, 'signin');
+initFormSubmit(formSignUp, 'https://neto-api.herokuapp.com/signup', messageSignUp, 'signup');
+
+function initFormSubmit(form, urlPOST, messageBlock, type) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const dataToObj = {};
+
+    for (const [key, value] of formData) {
+      dataToObj[key] = value;
+    }
+
+    const request = fetch(urlPOST, {
+      body: JSON.stringify(dataToObj),
+      credentials: 'same-origin',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    });
+
+    request
+      .then((result) => {
+        if (200 <= result.status && result.status < 300) {
+          return result;
+        }
+        throw new Error(response.statusText);
+      })
+      .then((result) => result.json())
+      .then((data) => {
+        const messageOKText = type === 'signin' ? `Пользователь ${data.name} успешно авторизован` : `Пользователь ${data.name} успешно зарегистрирован`;
+        messageBlock.value = data.error ? data.message : messageOKText;
+      });
+
+  });
+}
